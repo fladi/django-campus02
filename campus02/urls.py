39946f9fa5127d240d7147d50b676ad083514e85
Michael Fladischer <FladischerMichael@fladi.at>
2015-10-27 21:09:41 +0100

    Add custom debug toolbar URL mount point.

--- campus02/urls.py
+++ campus02/urls.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -11,3 +12,10 @@ urlpatterns = patterns(
     url(r'^web/', include('campus02.web.urls', namespace='web')),
     url(r'^', include('campus02.base.urls', namespace='base')),
 )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
