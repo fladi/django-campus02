01257cd79f9d49ec48b90c0a1576990d0879d25c
Michael Fladischer <FladischerMichael@fladi.at>
2015-12-05 20:48:00 +0100

    Set correct STATIC_ROOT, MEDIA_URL and MEDIA_ROOT.

--- campus02/settings.py
+++ campus02/settings.py
@@ -150,10 +150,14 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/var/www/vhosts/campus02.fladi.at/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     '/usr/share/javascript/',
 )
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
