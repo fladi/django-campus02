cab45beb96f00c0fce6e329cc547ced9ab4d1934
Michael Fladischer <FladischerMichael@fladi.at>
2016-08-11 11:00:43 +0200

    Add NotFoundView to handle 404 errors.

--- campus02/base/views.py
+++ campus02/base/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'base/index.html'
+
+
+class NotFoundView(TemplateView):
+    template_name = 'base/404.html'
