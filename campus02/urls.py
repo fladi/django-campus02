56ee13d9d760ec8898c2f800bbfee89fbbf80c21
Michael Fladischer <FladischerMichael@fladi.at>
2016-09-09 13:12:52 +0200

    Serve media and static files from Django if DEBUG is on.

--- campus02/urls.py
+++ campus02/urls.py
@@ -4,6 +4,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^', include('django.contrib.auth.urls')),
@@ -15,5 +16,26 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns.append(
-        url(r'^__debug__/', include(debug_toolbar.urls))
+        url(
+            r'^__debug__/',
+            include(debug_toolbar.urls)
+        )
+    )
+    urlpatterns.append(
+        url(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {
+                'document_root': settings.MEDIA_ROOT,
+            }
+        )
+    )
+    urlpatterns.append(
+        url(
+            r'^static/(?P<path>.*)$',
+            serve,
+            {
+                'document_root': settings.STATIC_ROOT,
+            }
+        ),
     )
