e3bac9c0a655ae49d6e15b16894712f4edbc994b
Michael Fladischer <FladischerMichael@fladi.at>
2016-10-12 14:14:57 +0200

    Add ordering filters for primary key based api.

--- campus02/web/views/primarykey.py
+++ campus02/web/views/primarykey.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from rest_framework import viewsets
-from rest_framework.filters import DjangoObjectPermissionsFilter
+from rest_framework import viewsets, filters
 
 from .. import (
     models,
@@ -16,33 +15,47 @@ from ..serializers import (
 class MovieViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
+    filter_backends = (
+        filters.OrderingFilter,
+    )
+    ordering_fields = (
+        'title',
+        'released',
+        'runtime',
+    )
 
 
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Genre.objects.all()
     serializer_class = serializers.GenreSerializer
+    filter_backends = (
+        filters.OrderingFilter,
+    )
+    ordering_fields = (
+        'name',
+    )
 
 
 class WatchlistViewSet(viewsets.ModelViewSet):
     queryset = models.Watchlist.objects.all()
     serializer_class = serializers.WatchlistSerializer
-    filter_backends = [
-        DjangoObjectPermissionsFilter,
-    ]
-    permission_classes = [
+    filter_backends = (
+        filters.DjangoObjectPermissionsFilter,
+    )
+    permission_classes = (
         permissions.DjangoObjectPermissions,
-    ]
+    )
 
 
 class ResumeViewSet(viewsets.ModelViewSet):
     queryset = models.Resume.objects.all()
     serializer_class = serializers.ResumeSerializer
-    filter_backends = [
-        DjangoObjectPermissionsFilter,
-    ]
-    permission_classes = [
+    filter_backends = (
+        filters.DjangoObjectPermissionsFilter,
+    )
+    permission_classes = (
         permissions.DjangoObjectPermissions,
-    ]
+    )
 
 
 class RatingViewSet(viewsets.ModelViewSet):
