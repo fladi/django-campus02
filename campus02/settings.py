7e2798127146117b9e98b386619941f7a34ec90a
Michael Fladischer <FladischerMichael@fladi.at>
2016-10-20 11:28:42 +0200

    Use Limit/Offset as a default pagination system for DRF.

--- campus02/settings.py
+++ campus02/settings.py
@@ -186,6 +186,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
 
